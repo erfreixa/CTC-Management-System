f9f635047023071d086b7a78b9b30122a09b627e
Vishwa Perera <pereravishwa061@gmail.com>
2019-11-05 11:31:17 -0500

    ii

--- Back-End/Back End/src/back_end_package/Member.java
+++ Back-End/Back End/src/back_end_package/Member.java
@@ -3,12 +3,9 @@ package back_end_package;
 public class Member {
 
 	public Member() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
